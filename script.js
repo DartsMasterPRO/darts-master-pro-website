e116c0842e1902aaa654be72319760f317049cd9
DartsMasterPRO <specjal87@gmail.com>
2026-06-16 12:19:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Darts Master Pro landing page loaded 🎯");
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+
+    const installBtn = document.getElementById('installBtn');
+    installBtn.style.display = 'block';
+
+    installBtn.addEventListener('click', async () => {
+        deferredPrompt.prompt();
+
+        const { outcome } = await deferredPrompt.userChoice;
+
+        if (outcome === 'accepted') {
+            console.log('Aplikacja zainstalowana');
+        }
+
+        deferredPrompt = null;
+    });
+});
